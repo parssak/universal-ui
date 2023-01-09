6d141a2f1cf073584697e7543e65957512082ef2
parssak <parssak@gmail.com>
2023-01-08 20:12:42 -0500

    feat: cleaned up

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -7,39 +7,15 @@ function usePrefersDarkMode() {
   return useMedia(['(prefers-color-scheme: dark)'], [true], false);
 }
 
-const KEY = 'universal-ui:dark-mode';
-
 export const useDarkMode = () => {
   const prefersDarkMode = usePrefersDarkMode();
-  const [stored, setStored] = useState(
-    typeof window !== 'undefined' && window.localStorage.getItem(KEY) === 'true'
-  );
+  const [stored, setStored] = useState(false);
 
   const client = useClient();
-  // const config = useUniversalUIConfig();
 
   useEffect(() => {
-    // if (!config.ssr) {
-    //   setStored(prefersDarkMode);
-    //   return;
-    // }
-
     setStored(prefersDarkMode);
-    // if (client) {
-    //   return;
-    // }
-
-    // SSR only
-    // const t = setTimeout(() => setStored(prefersDarkMode), 0);
-
-    // return () => t && clearTimeout(t);
   }, [prefersDarkMode, client]);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.localStorage.setItem(KEY, stored.toString());
-    }
-  }, [stored]);
 
   return [stored] as const;
 };
