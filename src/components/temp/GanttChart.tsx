bfebc385f619e346134a0acb913cdc8e1b3ade1a
parssak <parssak@gmail.com>
2023-01-20 12:13:07 -0500

    feat: redacting

--- src/components/temp/GanttChart.tsx
+++ src/components/temp/GanttChart.tsx
@@ -26,75 +26,16 @@ interface LayoutNode {
   name: string;
 }
 
-const computeLayoutNodes = (items: Item[]): LayoutNode[] => {
-  // Sort Items!
-  const sortedItems = items.sort(
-    (a, b) => new Date(a.start).getTime() - new Date(b.start).getTime()
-  );
-
-  if (sortedItems.length === 0) {
-    return [];
-  }
-
-  const start = new Date(sortedItems[0].start);
-  const end = new Date(sortedItems[sortedItems.length - 1].end);
-
-  const layoutRangeMs = end.getTime() - start.getTime();
-
-  const layoutNodes: LayoutNode[] = sortedItems.map((item, index) => {
-    const startDate = new Date(item.start);
-    const endDate = new Date(item.end);
-    const width = (endDate.getTime() - startDate.getTime()) / layoutRangeMs;
-    const x = (startDate.getTime() - start.getTime()) / layoutRangeMs;
-    return {
-      id: `${index}`,
-      x,
-      width,
-      name: item.name,
-    };
-  });
-
-  return layoutNodes;
-};
-
 export const GanttChart = ({
   defaultItems,
   ...props
 }: React.HTMLAttributes<HTMLDivElement> & {
   defaultItems: Item[];
-}) => {
-  const [items, setItems] = useState(defaultItems);
-
-  const layoutNodes = useMemo(() => computeLayoutNodes(items), [items]);
-
-  const onRemove = (name: string) => {
-    setItems(items.filter(item => item.name !== name));
-  };
-
-  const scale = 40;
+}) => {;
 
   return (
     <div {...props}>
-      <div className="relative">
-        <div className="space-y-2 overflow-x-auto py-4">
-          {layoutNodes.map((node, i) => (
-            <div
-              key={i}
-              className="h-5 border transition-all duration-500 border-neutral-600/30  rounded relative whitespace-nowrap"
-              style={{
-                left: `${node.x * scale}rem`,
-                width: `${node.width * scale}rem`,
-                backgroundColor: getColorFromString(node.name),
-              }}
-              onClick={() => onRemove(node.name)}
-            >
-              <span className="absolute select-none top-0 bottom-0 grid place-items-center left-full text-left pl-2 text-sm text-neutral-600 ">
-                {node.name}
-              </span>
-            </div>
-          ))}
-        </div>
-      </div>
+      {/* redacted */}
     </div>
   );
 };
